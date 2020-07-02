833ea8c8be0ad8ff7c7b0a7ee3ecd0d51f8b9a73
thetechgirlgita <thetechgirlgita@gmail.com>
2020-07-02 06:28:57 +0530

    Convert Alphabet to ASCII code

---
+++ ASCII/ASCII.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	 char c;
+	 printf("Enter the alphabet which ASCII do you want : ");
+	 scanf("%c",&c);
+	 printf("The ASCII code of %c is : %d",c,c);
+	 return 0;
+}
