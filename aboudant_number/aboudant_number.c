3e7e983f1f2cafe379147bc7f3441aacb7e10fd0
thetechgirlgita <thetechgirlgita@gmail.com>
2020-09-08 16:23:31 +0530

    Find the aboundant number

---
+++ aboudant_number/aboudant_number.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+# include <math.h>
+
+int getSum(int n) {
+    
+    int sum = 0;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i==0) {
+            if (n / i == i){
+                sum = sum + i;
+            }
+            else {
+                sum = sum + i;
+                sum = sum + (n / i);
+            }
+        }
+    }
+    sum = sum - n;
+    return sum;
+}
+
+bool checkAbundant(int n) {
+    return getSum(n) > n;
+
+}
+
+int main() {
+    
+  int n;
+  
+  printf(" Input an integer number: ");
+  scanf("%d",&n);
+
+    checkAbundant(n)? printf(" The number is Abundant.\n") 
+    :printf(" The number is not Abundant.\n");
+    return 0;
+}
